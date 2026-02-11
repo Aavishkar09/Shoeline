6ae44b835f2439873b51c4956d3029746b6ef899
Aavishkar <baraskaraavishkar@gmail.com>
2026-02-11 12:50:25 +0530

    Refactor routing structure in App and Admin components to improve readability and organization

--- Shoeline/src/App.jsx
+++ Shoeline/src/App.jsx
@@ -1,7 +1,7 @@
 
 import './App.css'
 import Navbar from './Component/Navbar/Navbar'
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import {Routes,Route} from 'react-router-dom'
 import Shop from './Pages/Shop'
 import Product from './Pages/Product'
 import ShopCategory from './Pages/ShopCategory'
@@ -23,25 +23,20 @@ function App() {
 
   return (
     <div>
-      <BrowserRouter>
-        <Navbar/>
-        <Routes>
-          <Route path='/' element={<Shop/>}/>
-          <Route path='/mens' element={<ShopCategory banner={men_banner} category="men"/>}/>
-          <Route path='/womens' element={<ShopCategory banner={women_banner} category="women"/>}/>
-          <Route path='/kids' element={<ShopCategory banner={kid_banner} category="kid"/>}/>
-          <Route path="/product" element={<Product/>}>
-            <Route path=':productId' element={<Product/>}/>
-          </Route>
-          <Route path='/cart' element={<Cart/>}/>
-          <Route path='/checkout' element={<Checkout/>}/>
-          <Route path='/order-confirmation' element={<OrderConfirmation/>}/>
-          <Route path='/login' element={<LoginSignup/>}/>
-          <Route path='/verify-email' element={<VerifyEmail/>}/>
-        </Routes>
-        <Footer/>
-      </BrowserRouter>
-      
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Shop/>}/>
+        <Route path='/mens' element={<ShopCategory banner={men_banner} category="men"/>}/>
+        <Route path='/womens' element={<ShopCategory banner={women_banner} category="women"/>}/>
+        <Route path='/kids' element={<ShopCategory banner={kid_banner} category="kid"/>}/>
+        <Route path='/product/:productId' element={<Product/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/checkout' element={<Checkout/>}/>
+        <Route path='/order-confirmation' element={<OrderConfirmation/>}/>
+        <Route path='/login' element={<LoginSignup/>}/>
+        <Route path='/verify-email' element={<VerifyEmail/>}/>
+      </Routes>
+      <Footer/>
     </div>
   )
 }
--- Shoeline/src/main.jsx
+++ Shoeline/src/main.jsx
@@ -4,11 +4,14 @@ import App from './App.jsx'
 import './index.css'
 import ShopContextProvider from './Context/ShopContext.jsx'
 import { QueryProvider } from './providers/QueryProvider.jsx'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <QueryProvider>
     <ShopContextProvider>
-      <App/>
+      <BrowserRouter>
+        <App/>
+      </BrowserRouter>
     </ShopContextProvider>
   </QueryProvider>
 )
